6a93c657733fed93dcae711d0df3df4b64b2c847
Michael Forney <mforney@mforney.org>
2013-11-19 17:59:30 -0800

    Allow environment to override PKG_CONFIG and WAYLAND_SCANNER

--- config.mk
+++ config.mk
@@ -9,8 +9,8 @@ PKGCONFIGDIR    = $(LIBDIR)/pkgconfig
 CC              = gcc
 CPPFLAGS        = -D_GNU_SOURCE # Required for mkostemp
 CFLAGS          = -O2 -pipe -g
-PKG_CONFIG      = pkg-config
-WAYLAND_SCANNER = wayland-scanner
+PKG_CONFIG      ?= pkg-config
+WAYLAND_SCANNER ?= wayland-scanner
 
 ENABLE_STATIC   = 1
 ENABLE_SHARED   = 1
